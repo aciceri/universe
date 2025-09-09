4286aa09895ad30119f61e0884d8b8f186c0b88d
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-09 16:14:06 +0200

    Add home-manager integration

---
+++ modules/home-manager.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
+{
+  debug = true;
+  flake.modules.nixos.base = nixosArgs: {
+    imports = [
+      inputs.home-manager.nixosModules.home-manager
+    ];
+
+    secrets = config.users |> lib.mapAttrs' (username: _: lib.nameValuePair "nixos_password_${username}" { });
+    users.mutableUsers = false;
+    users.users =
+      config.users
+      |> lib.mapAttrs (
+        username: user: {
+          isNormalUser = true;
+          hashedPasswordFile = nixosArgs.config.age.secrets."nixos_password_${username}".path;
+        }
+      );
+
+    home-manager = {
+      useGlobalPkgs = true;
+      extraSpecialArgs.hasGlobalPkgs = true;
+      users =
+        config.users // { root = { }; }
+        |> lib.mapAttrs (
+          username: _: {
+            imports = [
+              {
+                home.stateVersion = nixosArgs.config.system.stateVersion;
+                home = {
+                  inherit username;
+                };
+                programs.home-manager.enable = true;
+                systemd.user.startServices = "sd-switch";
+              }
+              config.flake.modules.homeManager.base
+            ];
+          }
+        );
+    };
+  };
+}
