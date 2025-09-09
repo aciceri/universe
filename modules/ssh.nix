358f7b4eabed386a2f4c06bbb9edd5ab2261379c
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-09 16:15:13 +0200

    Set home-manager options for ssh

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 {
-  flake.modules.nixos.ssh = {
+  flake.modules.nixos.base = {
     services.openssh = {
       enable = true;
       settings.PasswordAuthentication = false;
@@ -10,5 +10,18 @@
       lib.flip lib.filterAttrs config.users (_: user: user.god)
       |> lib.mapAttrsToList (_: user: user.sshKeys)
       |> lib.concatLists;
+  };
+
+  flake.modules.homeManager.base = {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        setEnv.TERM = "xterm-256color";
+        compression = true;
+        identitiesOnly = true;
+        hashKnownHosts = false;
+      };
+    };
   };
 }
