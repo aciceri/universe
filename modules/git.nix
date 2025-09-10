67e97417b0217a634f1b5224e0273338faf8a0f4
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-10 17:51:15 +0200

    Add git settings

--- modules/git.nix
+++ modules/git.nix
@@ -1,9 +1,51 @@
+{ lib, ... }:
 {
-  flake.modules.homeManager.base = {
-    programs.git = {
-      enable = true;
-    };
+  flake.modules.homeManager.base = lib.mkMerge [
+    (
+      { config, pkgs, ... }:
+      {
+        programs.git = {
+          enable = true;
+          extraConfig = {
+            ui.color = "auto";
+            pull.rebase = true;
+            rebase.autostash = true;
 
-    programs.lazygit.enable = true;
-  };
+            user.signingKey = "${config.home.homeDirectory}/.ssh/id_ed25519";
+            gpg.format = "ssh";
+            commit.gpgsign = true;
+
+            core = {
+              fsmonitor = true;
+              untrackedcache = true;
+            };
+            fetch.writeCommitGraph = true;
+          };
+        };
+
+        programs.lazygit = {
+          enable = true;
+          settings = {
+            git.paging = {
+              colorArg = "always";
+              useConfig = false;
+              pager = "${pkgs.delta}/bin/delta --dark --paging=never";
+            };
+          };
+        };
+      }
+    )
+    (
+      { config, ... }:
+      lib.mkIf (config.home.username == "ccr") {
+        programs.git = {
+          userName = "Andrea Ciceri";
+          userEmail = "andrea.ciceri@autistici.org";
+          extraConfig = {
+            github.user = "aciceri";
+          };
+        };
+      }
+    )
+  ];
 }
