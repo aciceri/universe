e1af7398eabf721bc9d873cde89014e7f5e3d7e9
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-10 11:53:01 +0200

    Add foot module

---
+++ modules/foot.nix
@@ -0,0 +1,32 @@
+{
+  flake.modules.homeManager.pc = {
+    programs.foot = {
+      enable = true;
+      server.enable = true;
+      settings = {
+        main = {
+          login-shell = "yes";
+          dpi-aware = "no";
+          horizontal-letter-offset = "1";
+          pad = "1x1";
+        };
+        cursor = {
+          blink = true;
+        };
+        tweak = {
+          overflowing-glyphs = true;
+        };
+
+        key-bindings = {
+          scrollback-up-page = "Control+Shift+k";
+          scrollback-down-page = "Control+Shift+j";
+          search-start = "Control+Shift+s";
+        };
+
+        mouse = {
+          hide-when-typing = "yes";
+        };
+      };
+    };
+  };
+}
