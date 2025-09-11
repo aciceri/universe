80cf65c685600255c30612cc5b0637f9b0c05b88
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-11 10:11:30 +0000

    Add helix settings

--- modules/helix.nix
+++ modules/helix.nix
@@ -2,6 +2,34 @@
   flake.modules.homeManager.base = {
     programs.helix = {
       enable = true;
+      defaultEditor = true;
+      settings = {
+        editor = {
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+          color-modes = true;
+          true-color = true;
+        };
+      };
+      languages = {
+        language = [
+          {
+            name = "nix";
+            language-servers = [ "nixd" ];
+            formatter.command = "nixfmt";
+          }
+          {
+            name = "typescript";
+            language-servers = [ "vtsls" ];
+          }
+        ];
+        language-server = {
+          nixd.command = "nixd";
+        };
+      };
     };
   };
 }
