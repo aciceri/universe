1d5b50cee932cf306f4da95ed81c188744118bea
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-11 10:43:23 +0200

    Add hm-activate utility

--- modules/universe.nix
+++ modules/universe.nix
@@ -13,9 +13,18 @@ fpArgs: {
         uri = "ssh://forgejo@git.aciceri.dev/aciceri/universe.git";
         interval = 1000;
       };
+
       nix.registry.universe.to = {
         type = "path";
         path = config.services.git-fetch.repositories.universe.path;
       };
+
+      programs.nushell.extraConfig = ''
+        def hm-activate [] {
+          let host = (hostname | str trim)
+          let user = (whoami | str trim)
+          nix run $"universe#nixosConfigurations.\"($host)\".config.home-manager.users.\"($user)\".home.activationPackage"
+        }
+      '';
     };
 }
