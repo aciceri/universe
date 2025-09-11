36302d9325ba05359aca6edbf08b61f671ce0f7a
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-11 09:43:02 +0200

    Use the git-fetch module to clone the universe repository

---
+++ modules/universe.nix
@@ -0,0 +1,17 @@
+fpArgs: {
+  flake.modules.nixos.workstation = {
+    programs.ssh.knownHosts = {
+      "git.aciceri.dev".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPBaKPx0HsJpGmMT//vo2GXvGh4ULoItq49ltCMzVw4";
+    };
+  };
+
+  flake.modules.homeManager.base =
+    { config, ... }:
+    {
+      services.git-fetch.repositories.universe = {
+        path = "${config.home.homeDirectory}/universe";
+        uri = "ssh://forgejo@git.aciceri.dev/aciceri/universe.git";
+        interval = 1000;
+      };
+    };
+}
