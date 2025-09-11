0a7eb22c5dcce22a9f04c5b8fc4e0b3baaca7441
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-11 15:23:51 +0000

    Add wireguard module
    Currently picard is connected to sisko (deployed from nixfleet)

---
+++ modules/wireguard.nix
@@ -0,0 +1,103 @@
+{ lib, ... }:
+{
+  flake.modules.nixos.base =
+    { config, pkgs, ... }:
+    let
+      cfg = config.wireguard;
+      inherit (config.networking) hostName;
+      hostSubmoduleType = lib.types.submodule (
+        { name, ... }:
+        {
+          options = {
+            name = lib.mkOption {
+              type = lib.types.str;
+              default = name;
+            };
+            ip = lib.mkOption {
+              type = lib.types.str;
+            };
+            publicKey = lib.mkOption {
+              type = lib.types.str;
+            };
+          };
+        }
+      );
+    in
+    {
+      options.wireguard = {
+        isClient = lib.mkOption {
+          type = lib.types.bool;
+          default = hostName != "sisko";
+        };
+        interfaceName = lib.mkOption {
+          type = lib.types.str;
+          default = "wg0";
+        };
+        publicKey = lib.mkOption {
+          type = lib.types.str;
+          default = "wg0";
+        };
+        hosts = lib.mkOption {
+          type = lib.types.attrsOf hostSubmoduleType;
+          default = { };
+        };
+      };
+      config = lib.mkMerge [
+        {
+          wireguard.hosts = {
+            sisko = {
+              ip = "10.100.0.1";
+              publicKey = "bc5giljukT1+ChbbyTLdOfejfR3c8RZ4XoXmQM54nTY=";
+            };
+            picard = {
+              ip = "10.100.0.2";
+              publicKey = "O9V2PI7+vZm7gGn3f9SaTsJbVe9urf/jZkdXFz/mjVU=";
+            };
+          };
+
+          secrets."wireguard_private_key_${hostName}" = { };
+          networking.firewall.trustedInterfaces = [ cfg.interfaceName ];
+          networking.wireguard.interfaces.${cfg.interfaceName} = {
+            mtu = 1200;
+            privateKeyFile = config.age.secrets."wireguard_private_key_${hostName}".path;
+            listenPort = 51820;
+          };
+        }
+        (lib.mkIf cfg.isClient {
+          networking.wireguard.interfaces.wg0 = {
+            ips = [ "${cfg.hosts.${hostName}.ip}/32" ];
+            peers = [
+              {
+                publicKey = cfg.hosts.sisko.publicKey;
+                allowedIPs = [ "10.100.0.0/24" ];
+                # allowedIPs = [ "0.0.0.0/24" ]; # Uncomment for full tunnel
+                endpoint = "vpn.aciceri.dev:51820";
+                persistentKeepalive = 25;
+              }
+            ];
+          };
+        })
+        (lib.mkIf (!cfg.isClient) {
+          networking.nat.enable = true;
+
+          networking.firewall.allowedUDPPorts = [ config.networking.wireguard.interfaces.wg0.listenPort ];
+
+          networking.wireguard.interfaces.wg0 = {
+            ips = [ "${cfg.hosts.${hostName}.ip}/24" ];
+            peers = lib.mapAttrsToList (_: host: {
+              publicKey = host.publicKey;
+              allowedIPs = [ "${host.ip}/32" ];
+            }) cfg.hosts;
+
+            postSetup = ''
+              ${lib.getExe' pkgs.iptables "iptables"} -t nat -A POSTROUTING -s 10.100.0.0/24 -o enP4p65s0 -j MASQUERADE
+            '';
+
+            postShutdown = ''
+              ${lib.getExe' pkgs.iptables "iptables"} -t nat -D POSTROUTING -s 10.100.0.0/24 -o enP4p65s0 -j MASQUERADE
+            '';
+          };
+        })
+      ];
+    };
+}
