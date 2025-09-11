b5d7bc757e208c51b9e30c4fab6f1eb883c69afb
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-11 14:15:12 +0000

    Add style for waybar

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -114,6 +114,127 @@
               };
             };
           };
+        style = ''
+          * {
+            font-size: 1.35rem;
+              font-weight: 600;
+              font-family: "Iosevka Comfy", "Nerd Font";
+            }
+
+            window#waybar {
+              border-top-right-radius: 5px;
+              border-bottom-right-radius: 5px;
+            }
+
+            .modules-left,
+            .modules-right,
+            .modules-center {
+              padding: 12px 4px;
+            }
+
+            tooltip,
+            tooltip * {
+              text-shadow: none;
+            }
+
+            #custom-sep {
+              color: @base03;
+            }
+
+            #workspaces button {
+              color: @base04;
+              background: none;
+              padding: 0;
+            }
+
+            #workspaces button:hover {
+              color: @base0B;
+              box-shadow: inherit;
+              text-shadow: inherit;
+            }
+
+            #workspaces button.focused,
+            #workspaces button.active {
+              color: @base0E;
+              border-bottom-color: @base0E;
+            }
+
+            #temperature {
+              color: @base0A;
+            }
+
+            #clock {
+              font-weight: 600;
+              color: @base0C;
+            }
+
+            #custom-bluetooth_devices {
+              color: @base0D;
+            }
+
+            #pulseaudio,
+            #wireplumber,
+            #sndio {
+              color: @base0B;
+            }
+
+            #pulseaudio.muted,
+            #wireplumber.muted,
+            #sndio.muted {
+              color: @base08;
+            }
+
+            #custom-powermenu {
+              margin: 12px 0 0 0;
+              color: @base04;
+            }
+
+            #upower,
+            #battery {
+              color: @base0A;
+            }
+
+            #upower.charging,
+            #battery.charging {
+              color: @base0B;
+            }
+
+            #network {
+              color: @base0D;
+            }
+
+            #network.disconnected {
+              color: @base08;
+            }
+
+            #backlight {
+              color: @base09;
+            }
+
+            #cpu {
+              color: @base0E;
+            }
+
+            #disk {
+              color: @base0C;
+            }
+
+            #mpd {
+              color: @base0E;
+            }
+
+            #memory {
+              color: @base08;
+            }
+
+            #bluetooth {
+              color: @base0D;
+            }
+
+            #bluetooth.disabled {
+              color: @base03;
+            }
+        '';
       };
 
       home.packages = [
