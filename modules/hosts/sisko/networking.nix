410e0079f2a9aadecff3d35a4dbfbd3dafd84c82
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-13 10:46:49 +0200

    Enable resolved and use the local adguard as nameserver

--- modules/hosts/sisko/networking.nix
+++ modules/hosts/sisko/networking.nix
@@ -1,5 +1,7 @@
 {
   configurations.nixos.sisko.module = {
     networking.hostName = "sisko";
+    networking.nameservers = [ "127.0.0.1" ]; # i.e. adguardhome
+    services.resolved.enable = true;
   };
 }
