15d49552d7d327229eeb170fa0760982470f2397
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-13 10:45:50 +0200

    Uncomment the stylix wallpaper

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -8,10 +8,10 @@
       stylix = {
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-        # image = pkgs.fetchurl {
-        #   url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/os/nix-black-4k.png";
-        #   hash = "sha256-HRZYeKDmfA53kb3fZxuNWvR8cE96tLrqPZhX4+z4lZA=";
-        # };
+        image = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/os/nix-black-4k.png";
+          hash = "sha256-HRZYeKDmfA53kb3fZxuNWvR8cE96tLrqPZhX4+z4lZA=";
+        };
         cursor = {
           package = pkgs.apple-cursor;
           name = "macOS";
