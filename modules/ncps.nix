832dea7949adf117c526c078b203835c37debc88
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-15 17:25:16 +0200

    Enable ncps

---
+++ modules/ncps.nix
@@ -0,0 +1,41 @@
+{
+  configurations.nixos.sisko.module =
+    let
+      dir = "/mnt/hd/ncps";
+    in
+    {
+      systemd.tmpfiles.rules = [
+        "d ${dir} 770 ncps ncps"
+      ];
+
+      services.ncps = {
+        enable = true;
+        cache = {
+          hostName = "ncps.sisko.wg.aciceri.dev";
+          dataPath = "${dir}/data";
+          tempPath = "${dir}/tmp";
+          databaseURL = "sqlite:${dir}/db/db.sqlite";
+          maxSize = "200G";
+          lru.schedule = "0 2 * * *"; # Clean up daily at 2 AM
+          allowPutVerb = true;
+          allowDeleteVerb = true;
+        };
+        server.addr = "0.0.0.0:8501";
+        upstream = {
+          caches = [
+            "https://cache.nixos.org"
+            "https://nix-community.cachix.org"
+            "https://mlabs.cachix.org"
+            "http://sisko.wg.aciceri.dev:8081/nixfleet"
+          ];
+          publicKeys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "mlabs.cachix.org-1:gStKdEqNKcrlSQw5iMW6wFCj3+b+1ASpBVY2SYuNV2M="
+            "nixfleet:Bud23440n6mMTmgq/7U+mk91zlLjnx2X3lQQrCBCCU4="
+          ];
+        };
+        prometheus.enable = true;
+      };
+    };
+}
