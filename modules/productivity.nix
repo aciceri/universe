3a3330a2b68ca1f3e1be36566107ecbf2e84f062
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-15 15:51:13 +0200

    Add claude-desktop

--- modules/productivity.nix
+++ modules/productivity.nix
@@ -1,6 +1,10 @@
+{ inputs, ... }:
 {
   flake.modules.homeManager.workstation =
     { pkgs, ... }:
+    let
+      nix-ai-tools = inputs.nix-ai-tools.packages.${pkgs.system};
+    in
     {
       home.packages = with pkgs; [
         # 2d stuff
@@ -36,6 +40,7 @@
         tremotesf
         vial
         trilium-next-desktop
+        nix-ai-tools.claude-desktop
       ];
     };
 }
