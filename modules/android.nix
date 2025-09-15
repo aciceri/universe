f8900e5cf80967951be9dfa9752022ee4bfff831
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-15 13:33:02 +0200

    Puts everything needed into the PATH

--- modules/android.nix
+++ modules/android.nix
@@ -15,8 +15,8 @@
   flake.modules.homeManager.workstation =
     { pkgs, ... }:
     let
-      reinstall-magisk-on-lineage = pkgs.stdenv.mkDerivation {
-        name = "reinstall-magisk-on-lineage";
+      patchedSrc = pkgs.stdenv.mkDerivation {
+        name = "patched-reinstall-magisk-on-lineageos-source";
         version = "git";
         src = pkgs.fetchFromGitHub {
           owner = "NicolasWebDev";
@@ -24,10 +24,6 @@
           rev = "1ca911ed555d4badd705c6c71750b78be8962b0b";
           hash = "sha256-95LzcWL4efR77i8UlzIT+7wQXp+91K2sUwcjmHvTf+Q=";
         };
-        propagatedBuildInputs = with pkgs; [
-          android-tools
-          jq
-        ];
         installPhase = ''
           mkdir -p $out/bin
           cp reinstall-magisk-on-lineageos $out/bin/reinstall-magisk-on-lineageos
@@ -37,8 +33,19 @@
             --replace-fail "paste_yours_here" "\"\$1\""
         '';
       };
+      path = lib.makeBinPath (
+        with pkgs;
+        [
+          android-tools
+          jq
+        ]
+      );
+      reinstall-magisk-on-lineageos = pkgs.writeShellScriptBin "reinstall-magisk-on-lineageos" ''
+        export PATH=$PATH:${path}
+        exec ${patchedSrc}/bin/reinstall-magisk-on-lineageos $1
+      '';
     in
     {
-      home.packages = [ reinstall-magisk-on-lineage ];
+      home.packages = [ reinstall-magisk-on-lineageos ];
     };
 }
