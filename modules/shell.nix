3d63721c71ff26b7421c79024536d15648899f21
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-18 22:03:39 +0200

    Add repgrep

--- modules/shell.nix
+++ modules/shell.nix
@@ -6,54 +6,61 @@
       programs.mosh.enable = true;
     };
 
-  flake.modules.homeManager.base = {
-    programs = {
-      nushell = {
-        enable = true;
-        settings = {
-          show_banner = false;
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      programs = {
+        nushell = {
+          enable = true;
+          settings = {
+            show_banner = false;
+          };
+        };
+
+        carapace.enable = true;
+
+        bat.enable = true;
+
+        fzf.enable = true;
+
+        fd.enable = true;
+
+        ripgrep.enable = true;
+
+        ripgrep-all.enable = true;
+
+        broot.enable = true;
+
+        direnv = {
+          enable = true;
+          config = {
+            warn_timeout = "60s";
+          };
+          nix-direnv.enable = true;
+        };
+
+        pay-respects.enable = true;
+
+        television.enable = true;
+
+        ranger = {
+          enable = true;
+          settings = {
+            "preview_images" = true;
+            "preview_images_method" = "sixel";
+          };
+        };
+
+        zoxide.enable = true;
+
+        btop = {
+          enable = true;
+          settings.update_ms = 100;
         };
       };
 
-      carapace.enable = true;
-
-      bat.enable = true;
-
-      fzf.enable = true;
-
-      fd.enable = true;
-
-      ripgrep.enable = true;
-      ripgrep-all.enable = true;
-
-      broot.enable = true;
-
-      direnv = {
-        enable = true;
-        config = {
-          warn_timeout = "60s";
-        };
-        nix-direnv.enable = true;
-      };
-
-      pay-respects.enable = true;
-
-      television.enable = true;
-
-      ranger = {
-        enable = true;
-        settings = {
-          "preview_images" = true;
-          "preview_images_method" = "sixel";
-        };
-      };
-
-      zoxide.enable = true;
-
-      btop = {
-        enable = true;
-        settings.update_ms = 100;
-      };
+      home.packages = with pkgs; [
+        repgrep
+      ];
     };
-  };
 }
