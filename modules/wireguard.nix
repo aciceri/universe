88e8398cdf2648f91b0d72b2a1444cb8cae62fa4
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-23 15:24:15 +0200

    Add MLabs VPN configuration

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -112,4 +112,23 @@
         })
       ];
     };
+
+  flake.modules.nixos.workstation =
+    { config, ... }:
+    {
+      secrets.wireguard_mlabs_private_key = { }; # the same for all the workstations, usually only one at time is on
+      networking.wireguard.interfaces.wg1 = {
+        ips = [ "10.10.1.1/32" ];
+        peers = [
+          {
+            publicKey = "A4u2Rt5WEMHOAc6YpDABkqAy2dzzFLH9Gn8xWcKaPQQ=";
+            allowedIPs = [ "10.10.0.0/16" ];
+            endpoint = "vpn.staging.mlabs.city:51820";
+            persistentKeepalive = 25;
+          }
+        ];
+        privateKeyFile = config.age.secrets.wireguard_mlabs_private_key.path;
+        mtu = 1300;
+      };
+    };
 }
