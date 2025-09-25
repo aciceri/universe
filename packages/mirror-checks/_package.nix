bfb79c2f922066a10cf1ca1d9ec28225525ff061
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-25 21:44:49 +0200

    Add meta attributes

--- packages/mirror-checks/_package.nix
+++ packages/mirror-checks/_package.nix
@@ -1,7 +1,25 @@
-{ writers, python3Packages }:
-writers.writePython3Bin "mirror-checks" {
-  libraries = with python3Packages; [
-    requests
-    pygithub
-  ];
-} (builtins.readFile ./mirror-checks.py)
+{
+  writers,
+  python3Packages,
+  lib,
+}:
+let
+  script = writers.writePython3Bin "mirror-checks" {
+    libraries = with python3Packages; [
+      requests
+      pygithub
+    ];
+  } (builtins.readFile ./mirror-checks.py);
+in
+script.overrideAttrs (oldAttrs: {
+  meta =
+    with lib;
+    oldAttrs.meta or { }
+    // {
+      description = "Utility to synchronize CI checks from Forgejo to GitHub";
+      license = licenses.gpl3Plus;
+      maintainers = [ maintainers.aciceri ];
+      sourceProvenance = with sourceTypes; [ fromSource ];
+      # mainProgram is set automatically by writePython3Bin
+    };
+})
