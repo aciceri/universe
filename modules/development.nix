287165c144c6e00d6d6c92ff75e1103455ac52e6
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-29 21:17:37 +0200

    Add nixpkgs-review helper

--- modules/development.nix
+++ modules/development.nix
@@ -11,6 +11,8 @@
         nixpkgs-review
         nurl
         nix-fast-build
+        nix-update
+        nix-melt
       ];
 
       programs.gh = {
@@ -22,5 +24,13 @@
       };
 
       programs.gh-dash.enable = true;
+
+      programs.nushell.extraConfig = ''
+        def nixpkgs-review-and-post [pr: int] {
+          let ssh_key = $"($env.HOME)/.ssh/id_ed25519"
+          let builders = $"ssh-ng://aciceri@darwin-build-box.nix-community.org x86_64-darwin,aarch64-darwin ($ssh_key); ssh-ng://root@sisko.wg.aciceri.dev aarch64-linux ($ssh_key)"
+          nixpkgs-review pr ($pr) --build-args=$"--builders '($builders)'" --post-result --systems all
+         }
+      '';
     };
 }
