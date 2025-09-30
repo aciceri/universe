7dd96d26397c3da8a7eff390d177d200e3dfa45c
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-30 15:37:45 +0200

    Fix passthru override

--- packages/yt-dlp-master/_package.nix
+++ packages/yt-dlp-master/_package.nix
@@ -14,8 +14,10 @@ yt-dlp.overrideAttrs (
       rev = "4429fd0450a3fbd5e89573e06533c1a0874fae42";
       hash = "sha256-/uzs87Vw+aDNfIJVLOx3C8RyZvWLqjggmnjrOvUX1Ea=";
     };
-    passthru.updateScript = nix-update-script {
-      extraArgs = [ "--version=branch" ];
+    passthru = previousAttrs.passthru // {
+      updateScript = nix-update-script {
+        extraArgs = [ "--version=branch" ];
+      };
     };
     meta.maintainers = previousAttrs.meta.maintainers ++ [ lib.maintainers.aciceri ];
   }
