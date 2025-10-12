c4421a19c97bfcea28b4127f62203f82441fb7bd
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-10-12 12:41:54 +0200

    Use helix-master and improve configuration

--- modules/helix.nix
+++ modules/helix.nix
@@ -1,77 +1,120 @@
+{ lib, ... }:
 {
-  flake.modules.homeManager.base = {
-    programs.helix = {
-      enable = true;
-      defaultEditor = true; # doesn't seem to work with nushell
-      settings = {
-        editor = {
-          mouse = true;
-          middle-click-paste = true;
-          scroll-lines = 3;
-          color-modes = true;
-          true-color = true;
-          rulers = [ ];
-          bufferline = "never";
-          cursorline = true;
-          auto-save = false;
-          line-number = "relative";
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      programs.helix = {
+        enable = true;
+        package = pkgs.helix-master;
+        defaultEditor = true; # doesn't seem to work with nushell
+        settings = {
+          editor = {
+            mouse = true;
+            middle-click-paste = true;
+            scroll-lines = 3;
+            color-modes = true;
+            true-color = true;
+            rulers = [ ];
+            bufferline = "never";
+            cursorline = true;
+            line-number = "relative";
+            rainbow-brackets = true;
+            completion-replace = true;
+            trim-trailing-whitespace = true;
+            end-of-line-diagnostics = "hint";
 
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
-          };
+            inline-diagnostics.cursor-line = "warning";
 
-          statusline = {
-            left = [
-              "mode"
-              "spinner"
-              "file-name"
-            ];
-            center = [ ];
-            right = [
-              "diagnostics"
-              "selections"
-              "position"
-              "file-encoding"
-              "file-line-ending"
-            ];
-          };
+            soft-wrap.enable = true;
 
-          lsp = {
-            display-messages = true;
-            auto-signature-help = true;
-            display-inlay-hints = true;
-          };
+            auto-save = {
+              focus-lost = true;
+              after-delay.enable = true;
+              after-delay.timeout = 300000;
+            };
 
-          file-picker = {
-            hidden = true;
-            git-ignore = true;
-          };
+            cursor-shape = {
+              insert = "bar";
+              normal = "block";
+              select = "underline";
+            };
 
-          indent-guides = {
-            render = true;
+            statusline = {
+              left = [
+                "mode"
+                "spinner"
+                "version-control"
+                "spacer"
+                "separator"
+                "file-name"
+                "read-only-indicator"
+                "file-modification-indicator"
+              ];
+              center = [ ];
+              right = [
+                "diagnostics"
+                "workspace-diagnostics"
+                "position"
+                "total-line-numbers"
+                "position-percentage"
+                "file-encoding"
+                "file-line-ending"
+                "file-type"
+                "register"
+                "selections"
+              ];
+              separator = "│";
+            };
+
+            lsp = {
+              display-messages = true;
+              auto-signature-help = true;
+              display-inlay-hints = true;
+            };
+
+            file-picker = {
+              hidden = true;
+              git-ignore = true;
+            };
+
+            indent-guides = {
+              render = true;
+            };
           };
         };
-      };
-      languages = {
-        language = [
-          {
-            name = "nix";
-            language-servers = [ "nixd" ];
-            formatter.command = "nixfmt";
-          }
-          {
-            name = "typescript";
-            language-servers = [ "vtsls" ];
-          }
-        ];
-        language-server = {
-          nixd.command = "nixd";
+        languages = {
+          language-server = {
+            harper-ls = {
+              command = lib.getExe pkgs.harper;
+              args = [ "--stdio" ];
+              config.SentenceCapitalization = false;
+            };
+            nixd = {
+              command = lib.getExe pkgs.nixd;
+              args = [
+                "--inlay-hints"
+                "--semantic-tokens"
+              ];
+            };
+          };
+
+          language = [
+            {
+              name = "nix";
+              language-servers = [
+                "nixd"
+                "harper-ls"
+              ];
+              formatter.command = "nixfmt";
+            }
+            {
+              name = "typescript";
+              language-servers = [ "vtsls" ];
+            }
+          ];
         };
       };
+
+      programs.nushell.environmentVariables.EDITOR = "hx";
     };
-
-    programs.nushell.environmentVariables.EDITOR = "hx";
-  };
 }
