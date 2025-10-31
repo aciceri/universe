75f0668f2933b73207157fa07c520b6f5a650567
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-10-31 21:53:35 +0100

    Set stylix polarity

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -8,6 +8,7 @@
       stylix = {
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+        polarity = "dark";
         image = pkgs.fetchurl {
           url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/os/nix-black-4k.png";
           hash = "sha256-HRZYeKDmfA53kb3fZxuNWvR8cE96tLrqPZhX4+z4lZA=";
