be4444816168c9abb267eabf588cead6fc6d1830
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-11-20 11:35:58 +0100

    Remove bookmarks

--- modules/zen-browser.nix
+++ modules/zen-browser.nix
@@ -186,26 +186,7 @@ fpArgs@{ inputs, ... }:
 
           bookmarks = {
             force = true;
-            settings = [
-              {
-                name = "Nix sites";
-                toolbar = true;
-                bookmarks = [
-                  {
-                    name = "homepage";
-                    url = "https://nixos.org/";
-                  }
-                  {
-                    name = "wiki";
-                    tags = [
-                      "wiki"
-                      "nix"
-                    ];
-                    url = "https://wiki.nixos.org/";
-                  }
-                ];
-              }
-            ];
+            settings = [ ];
           };
 
           containersForce = true;
