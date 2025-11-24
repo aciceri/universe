b629492dee915b4e254ff681d7b4f2552f6aeff2
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-11-24 22:32:35 +0100

    Use chrony instead of timesyncd

--- modules/timezone.nix
+++ modules/timezone.nix
@@ -3,7 +3,15 @@
   flake.modules.nixos.base = {
     services.automatic-timezoned.enable = true;
     services.geoclue2.enable = true;
-    services.timesyncd.enable = lib.mkDefault true;
+    services.chrony = {
+      enable = lib.mkDefault true;
+      servers = [
+        "0.nixos.pool.ntp.org"
+        "1.nixos.pool.ntp.org"
+        "2.nixos.pool.ntp.org"
+        "3.nixos.pool.ntp.org"
+      ];
+    };
   };
 
   configurations.nixos.sisko.module = {
