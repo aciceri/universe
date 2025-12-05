5699c34793b4ffd3d35a8351e58fadec6c4690cc
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-12-05 22:06:10 +0100

    Part 2 of day 2

--- projects/aoc/2025/src/Day02.hs
+++ projects/aoc/2025/src/Day02.hs
@@ -1,4 +1,4 @@
-module Day02 (sumWrongIds, isRepetitive, part1, part2) where
+module Day02 (part1, part2) where
 
 import Data.Void (Void)
 import Text.Megaparsec (ParseErrorBundle, Parsec, parse, sepBy)
@@ -44,20 +44,25 @@ sumWrongIds ranges = sum $ sumWrongIds' <$> ranges
 part1 :: String -> Either (ParseErrorBundle String Void) Int
 part1 input = sumWrongIds <$> parseRanges input
 
--- isRepetitivePart2 :: Int -> Bool
--- isRepetitivePart2 n
---   | odd digits = False
---   | otherwise  = n `mod` ((10^(digits `div` 2)) + 1) == 0
---   where digits = numDigits n
+getDivisors :: Int -> [Int]
+getDivisors n = [m | m <- [2 .. n], n `mod` m == 0]
 
--- sumWrongIdsPart2' :: Range -> Int
--- sumWrongIdsPart2' (Range min max) = sum $ filter isRepetitive [min..max]
+repeatNum :: Int -> Int -> Int
+repeatNum n times = read $ concat $ replicate times $ show n
 
--- sumWrongIdsPart2 :: [Range] -> Int
--- sumWrongIdsPart2 ranges = sum $ sumWrongIdsPart2' <$> ranges
+firstDigits :: Int -> Int -> Int
+firstDigits n num = read $ take n $ show num
+
+isRepetitivePart2 :: Int -> Bool
+isRepetitivePart2 n = any (\d -> repeatNum (firstDigits (digits `div` d) n) d == n) $ getDivisors digits
+  where
+    digits = numDigits n
+
+sumWrongIdsPart2' :: Range -> Int
+sumWrongIdsPart2' (Range min max) = sum $ filter isRepetitivePart2 [min .. max]
+
+sumWrongIdsPart2 :: [Range] -> Int
+sumWrongIdsPart2 ranges = sum $ sumWrongIdsPart2' <$> ranges
 
 part2 :: String -> Either (ParseErrorBundle String Void) Int
-part2 = part1
-
--- part2 :: String -> Either (ParseErrorBundle String Void) Int
--- part2 input = sumWrongIdsPart2 <$> parseRanges input
+part2 input = sumWrongIdsPart2 <$> parseRanges input
--- projects/aoc/2025/test/Spec.hs
+++ projects/aoc/2025/test/Spec.hs
@@ -26,7 +26,7 @@ daySpec n p1 e1 p2 e2 = describe ("Day" ++ show n) $ do
 main :: IO ()
 main = hspec $ do
   daySpec 1 Day01.part1 1147 Day01.part2 6789
-  daySpec 2 Day02.part1 54641809925 Day02.part1 54641809925 -- TODO part 2
+  daySpec 2 Day02.part1 54641809925 Day02.part2 73694270688
   daySpec 3 Day03.part1 16887 Day03.part1 16887 -- TODO part 2
   daySpec 4 Day04.part1 1435 Day04.part2 8623
   daySpec 5 Day05.part1 661 Day05.part2 359526404143208
