a9e9d06aee34f57c37d970e3d64127143495aa4d
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-03-13 14:14:36 +0100

    Join zerotier network

--- modules/zerotier.nix
+++ modules/zerotier.nix
@@ -2,7 +2,10 @@
   flake.modules.nixos.base = {
     services.zerotierone = {
       enable = true;
-      joinNetworks = [ "632ea29085af0cb4" ];
+      joinNetworks = [
+        "632ea29085af0cb4" # universe
+        "096e27c0f271be6b" # addictive clan
+      ];
     };
   };
 
