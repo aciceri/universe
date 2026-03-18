d610f0ff31d7480a5d5b18dfc30c1721b738d0c5
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-03-18 11:21:49 +0100

    Add fail2ban

---
+++ modules/fail2ban.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.nixos.base = {
+    services.fail2ban = {
+      enable = true;
+      maxretry = 5;
+      bantime = "1h";
+      bantime-increment.enable = true;
+    };
+  };
+
+  configurations.nixos.sisko.module = {
+    services.fail2ban.jails = {
+      nginx-botsearch.settings = {
+        enabled = true;
+        filter = "nginx-botsearch";
+        logpath = "/var/log/nginx/access.log";
+      };
+      nginx-bad-request.settings = {
+        enabled = true;
+        filter = "nginx-bad-request";
+        logpath = "/var/log/nginx/access.log";
+      };
+    };
+  };
+}
