4d722fe092fccd22417229214be6fce65ecd54ce
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-03-18 16:32:43 +0100

    Correctly power off sisko's display

--- hosts/sisko/boot.nix
+++ hosts/sisko/boot.nix
@@ -1,21 +1,35 @@
 {
-  configurations.nixos.sisko.module = {
-    boot = {
-      loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
+  configurations.nixos.sisko.module =
+    { pkgs, lib, ... }:
+    {
+      boot = {
+        loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+        };
+
+        initrd.availableKernelModules = [
+          "nvme"
+          "xhci_pci"
+          "usb_storage"
+          "thunderbolt"
+        ];
+
+        kernelParams = [
+          "consoleblank=300"
+        ];
       };
 
-      initrd.availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "usb_storage"
-        "thunderbolt"
-      ];
-
-      kernelParams = [
-        "consoleblank=300"
-      ];
+      systemd.services.console-dpms = {
+        description = "Enable DPMS power down on console";
+        after = [ "systemd-vconsole-setup.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.writeShellScript "console-dpms" ''
+            TERM=linux ${lib.getExe' pkgs.util-linux "setterm"} --powerdown 1 > /dev/tty1
+          ''}";
+        };
+      };
     };
-  };
 }
