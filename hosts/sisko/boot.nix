d91b31c849e81f2df5c0393178c194296f56abc6
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-03-19 12:19:35 +0100

    Fix display automatic shutdown on sisko

--- hosts/sisko/boot.nix
+++ hosts/sisko/boot.nix
@@ -1,6 +1,6 @@
 {
   configurations.nixos.sisko.module =
-    { pkgs, lib, ... }:
+    { pkgs, ... }:
     {
       boot = {
         loader = {
@@ -20,15 +20,34 @@
         ];
       };
 
-      systemd.services.console-dpms = {
-        description = "Enable DPMS power down on console";
-        after = [ "systemd-vconsole-setup.service" ];
+      # Sync the laptop panel backlight with the kernel's console blanking state.
+      # The i915/eDP driver ignores DPMS via setterm, so we poll fb0/blank and
+      # toggle the backlight through sysfs to actually turn the panel off/on.
+      systemd.services.backlight-sync = {
+        description = "Sync backlight with console blank state";
+        after = [ "multi-user.target" ];
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.writeShellScript "console-dpms" ''
-            TERM=linux ${lib.getExe' pkgs.util-linux "setterm"} --powerdown 1 > /dev/tty1
+          Type = "simple";
+          ExecStart = "${pkgs.writeShellScript "backlight-sync" ''
+            BACKLIGHT=/sys/class/backlight/intel_backlight/bl_power
+            FB_BLANK=/sys/class/graphics/fb0/blank
+            PREV=""
+            while true; do
+              STATE=$(cat "$FB_BLANK")
+              if [ "$STATE" != "$PREV" ]; then
+                if [ "$STATE" = "1" ]; then
+                  echo 1 > "$BACKLIGHT"
+                else
+                  echo 0 > "$BACKLIGHT"
+                fi
+                PREV="$STATE"
+              fi
+              sleep 2
+            done
           ''}";
+          Restart = "always";
+          RestartSec = 5;
         };
       };
     };
