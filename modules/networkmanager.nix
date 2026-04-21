fae76f0393226de7a563320b8a630a8eadb7950e
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-04-21 10:28:42 +0200

    Add ccr to the networkmanager group

--- modules/networkmanager.nix
+++ modules/networkmanager.nix
@@ -3,5 +3,7 @@
   flake.modules.nixos.pc = {
     networking.networkmanager.enable = true;
     networking.useDHCP = lib.mkForce true;
+
+    users.users.ccr.extraGroups = [ "networkmanager" ];
   };
 }
