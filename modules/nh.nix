acf82818930aa963fde2dbf0d4c41f9ba6e98083
Andrea Ciceri <andrea.ciceri@autistici.org>
2026-08-07 12:37:45 +0200

    Deliver NH_FLAKE to nushell sessions

--- modules/nh.nix
+++ modules/nh.nix
@@ -12,5 +12,8 @@
       };
 
       home.sessionVariables.NH_FLAKE = config.universePath;
+      # home.sessionVariables only reaches POSIX shells via hm-session-vars.sh;
+      # nushell (the login shell) needs the variable delivered explicitly.
+      programs.nushell.environmentVariables.NH_FLAKE = config.universePath;
     };
 }
